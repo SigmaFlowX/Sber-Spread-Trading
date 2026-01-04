e690c7a058e29c6bb95a304416087ddef8378ad3
Dmitrii Bakaev <bakaevdima4@gmail.com>
2026-01-04 21:33:44 +0800

    Added script to find current optimal parametrs for the strategy

---
+++ tune_parametrs.py
@@ -0,0 +1,10 @@
+# This code is used to find current optimal parameters for the strategy
+import strategy
+import optuna
+
+
+df = strategy.open_data(since="2025-06-01")
+study = optuna.create_study(direction="maximize")
+study.optimize(lambda trial: strategy.objective(trial, df), n_trials=200, n_jobs=8)
+best_params = study.best_params
+print(best_params)
