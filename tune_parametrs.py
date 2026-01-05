5aef7da93b8fae9d16fc05b04bb7ec616f71d891
Dmitrii Bakaev <bakaevdima4@gmail.com>
2026-01-05 18:44:28 +0800

    Little adjustment

--- tune_parametrs.py
+++ tune_parametrs.py
@@ -3,7 +3,7 @@ import strategy
 import optuna
 
 
-df = strategy.open_data(since="2025-06-01")
+df = strategy.open_data(timeframe=1, since="2025-06-01")
 study = optuna.create_study(direction="maximize")
 study.optimize(lambda trial: strategy.objective(trial, df), n_trials=200, n_jobs=8)
 best_params = study.best_params
