5079484685c24023f62637911f9a2dd3e9896078
Dmitrii Bakaev <bakaevdima4@gmail.com>
2026-01-05 12:28:09 +0800

    Added changing the timeframe easier and some minor improvements

--- get_moex_data.py
+++ get_moex_data.py
@@ -6,7 +6,11 @@ from datetime import datetime, timedelta
 def get_candles(symbol, start_date, end_date, interval=10):
     url = f"https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities/{symbol}/candles.json"
     cur_date = start_date
-    delta = 3 #дней, на которые разбиваем загрузку данных
+
+    if interval == 10:
+        delta = 3
+    else:
+        delta = 0.5
     df = pd.DataFrame()
 
     session = requests.Session()
@@ -29,7 +33,7 @@ def get_candles(symbol, start_date, end_date, interval=10):
 
     duplicates_count = df.duplicated(subset=["begin"]).sum()
     df.drop_duplicates(subset=["begin"], inplace=True)
-    print("Удалено дубликатов:", duplicates_count)
+    print("Number of deleted duplicates:", duplicates_count)
 
     df['timestamp'] = pd.to_datetime(df['begin'])
     df.set_index('timestamp', inplace=True)
@@ -40,11 +44,12 @@ def get_candles(symbol, start_date, end_date, interval=10):
 
 
 
+timeframe = 1
 symbol = "SBERP"
 start_date = datetime(2015, 1, 1)
-end_date = datetime(2025, 11, 1)
-df = get_candles(symbol, start_date, end_date, 10)
-df.to_csv("SBERP10min.csv")
+end_date = datetime(2025, 12, 31)
+df = get_candles(symbol, start_date, end_date, timeframe)
+df.to_csv(f"{symbol}{timeframe}min.csv")
 
 plt.plot(df.index, df['close'])
 plt.show()
