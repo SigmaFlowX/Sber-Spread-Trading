7daeee51b37f07eb95c99ca5c3548677cb3d18d3
Dmitrii Bakaev <bakaevdima4@gmail.com>
2026-02-01 18:45:23 +0800

    Minor changes due to renamed files

--- src/tune_parametrs.py
+++ src/tune_parametrs.py
@@ -1,10 +1,10 @@
 # This code is used to find current optimal parameters for the strategy
-from src import strategy
+from src import strategy_optimization
 import optuna
 
 
-df = strategy.open_data(timeframe=1, since="2025-06-01")
+df = strategy_optimization.open_data(timeframe=1, since="2025-06-01")
 study = optuna.create_study(direction="maximize")
-study.optimize(lambda trial: strategy.objective(trial, df), n_trials=200, n_jobs=8)
+study.optimize(lambda trial: strategy_optimization.objective(trial, df), n_trials=200, n_jobs=8)
 best_params = study.best_params
 print(best_params)
